f84a4e3ab7a3f2236a139d33fdd69a2ebe14e4d4
steven van eecke <stevenvaneecke@hotmail.com>
2018-11-08 11:03:31 +0800

    Added has_many :bookings, dependent: :destroy

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -1,4 +1,5 @@
 class Boat < ApplicationRecord
   belongs_to :user
+  has_many :bookings, dependent: :destroy
   validates :name, :type, :description, :location, :daily_price, :capacity, :crew_number, :photo, presence: true
 end
