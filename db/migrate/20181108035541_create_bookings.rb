9d3cc09ecc9a93f615756888fa3df8a061545f70
Andreas Van Assche <andreasava@hotmail.com>
2018-11-08 11:57:26 +0800

    fixed the bookings model

--- db/migrate/20181108035541_create_bookings.rb
+++ db/migrate/20181108035541_create_bookings.rb
@@ -1,8 +1,8 @@
 class CreateBookings < ActiveRecord::Migration[5.2]
   def change
     create_table :bookings do |t|
-      t.user :references
-      t.boat :references
+      t.references :user, foreign_key: true
+      t.references :boat, foreign_key: true
       t.date :start_date
       t.date :end_date
       t.string :status
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_08_024308) do
+ActiveRecord::Schema.define(version: 2018_11_08_035541) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,20 @@ ActiveRecord::Schema.define(version: 2018_11_08_024308) do
     t.index ["user_id"], name: "index_boats_on_user_id"
   end
 
+  create_table "bookings", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "boat_id"
+    t.date "start_date"
+    t.date "end_date"
+    t.string "status"
+    t.integer "passenger_number"
+    t.integer "total_price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["boat_id"], name: "index_bookings_on_boat_id"
+    t.index ["user_id"], name: "index_bookings_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -43,4 +57,6 @@ ActiveRecord::Schema.define(version: 2018_11_08_024308) do
   end
 
   add_foreign_key "boats", "users"
+  add_foreign_key "bookings", "boats"
+  add_foreign_key "bookings", "users"
 end
