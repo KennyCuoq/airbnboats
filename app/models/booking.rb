167bbe34fda0b6858e0de8388735a7c30215c5f4
Andreas Van Assche <andreasava@hotmail.com>
2018-11-08 11:39:30 +0800

    generated bookings model and controller

---
+++ app/models/booking.rb
@@ -0,0 +1,7 @@
+class Booking < ApplicationRecord
+  belongs_to :user
+ belongs_to :boat
+ validates :start_date, presence: true
+ validates :end_date, presence: true
+ validates :passenger_number, presence: true, numericality: { greater_than: 0 }
+end
