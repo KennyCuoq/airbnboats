5acb4ba3764e41d70326b9c60392bc4c16a0cc6a
Kenny Cuoq <kenny.cuoq@gmail.com>
2018-11-08 14:21:14 +0800

    Created back end for #new and #create action for Boats

--- app/controllers/boats_controller.rb
+++ app/controllers/boats_controller.rb
@@ -6,11 +6,24 @@ class BoatsController < ApplicationController
   end
 
   def new
+    @boat = Boat.new
   end
 
   def create
+    @boat = Boat.new(boat_params)
+    @boat.user = current_user
+    if @boat.save
+      redirect_to boat_path(@boat)
+    else
+      render :new
+    end
   end
 
   def destroy
+  end
+
+  private
+  def boat_params
+    params.require(:boat).permit(:name, :boat_type, :description, :location, :daily_price, :capacity, :crew_number, :photo)
   end
 end
