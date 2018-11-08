6a81bd725e5091a7aac1751322c64ac6b4a46006
Andreas Van Assche <andreasava@hotmail.com>
2018-11-08 18:04:41 +0800

    CSS

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -19,17 +19,25 @@ class BookingsController < ApplicationController
     @bookings = @boat.bookings
   end
 
-  def patch
+  def edit
+    @booking = Booking.find(params[:id])
     @boat = Boat.find(params[:boat_id])
-    @booking = Booking.find(booking_params)
   end
 
-  def edit
+  def update
+    @booking = Booking.find(params[:id])
+    @booking.update(booking_params)
+    @booking.total_price = (@booking.end_date - @booking.start_date).to_i * @booking.boat.daily_price
+    if @booking.save
+      redirect_to boat_booking_path(@booking.boat, @booking)
+    else
+      render "boats/show"
+    end
   end
 
   private
 
   def booking_params
-    params.require(:booking).permit(:start_date, :end_date, :passenger_number, :total_price)
+    params.require(:booking).permit(:start_date, :end_date, :passenger_number)
   end
 end
